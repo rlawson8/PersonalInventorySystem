1fd0d9ff740fa2600310fc68ae8fff3dfe23b477
root <root@localhost.localdomain>
2022-10-22 17:22:51 +0000

    Adding environment and Gunicorn functionality

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
 
 @app.route('/')
 def hello_world():
