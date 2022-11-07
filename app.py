a6e951a8a37de5cb5eb2618676550f5b1e09ad2d
JacksonDudenhoeffer <jackmattdude@gmail.com>
2022-11-07 16:57:29 -0600

    fixed spacing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from forms import registration, login
 app = Flask(__name__)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
 @app.route('/')
 def hello_world():
@@ -34,25 +34,25 @@ def log_in():
 @app.route('/createAccount', methods=['GET','POST'])
 def create_account():
     if request.method == 'POST':
-		username = request.form['username']
+        username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-		action = registration(username, email,  password)
+        action = registration(username, email,  password)
         #check here to see if the email and username has already been used by performing a query
         #if the above query is true flash the below message and direct back to the signup page using:
         #return redirect(url_for('app.createAccount'))
         #flash('Email address already exists')
         #create a new user with the form data and hash the password so the plaintext version isn't saved this is where we canc all the method in forms.py
         #add the new user to the database
-		if action == 409:
-			flash('Username already exists')
-			return render_template("register.html")
-		elif action == 410:
-			flash('Email address already exists')
-			return render_template("register.html")
-		else:
-			flash('Thank you for creating your account '+username)
-			return render_template("HomePage.html")
+        if action == 409:
+            flash('Username already exists')
+            return render_template("register.html")
+        elif action == 410:
+            flash('Email address already exists')
+            return render_template("register.html")
+        else:
+            flash('Thank you for creating your account '+username)
+            return render_template("HomePage.html")
     return render_template("register.html")
 
 @app.route('/design')
