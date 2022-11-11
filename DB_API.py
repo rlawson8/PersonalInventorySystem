123f3879562f16e88ffa7cf31efb73a6ff217114
Trevor Gentry <69729638+gigglebits@users.noreply.github.com>
2022-11-11 15:17:42 -0600

    DB config update

--- DB_API.py
+++ DB_API.py
@@ -17,15 +17,15 @@ pepper = "Mizzou2022!_IMT"
 def connectToDB():
     try:
         db = mariadb.connect(
-            #user="trevor",
-            #password="Graduation",
+            user="trevor",
+            password="Graduation",
             host="127.0.0.1",
-            port=3307,
-            #database="Tabs_DB"
+            port=3306,
+            database="Tabs_DB"
 
-            user="root",
-            password="root",
-            database="pis_db"
+            #user="root",
+            #password="root",
+            #database="pis_db"
         )
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
